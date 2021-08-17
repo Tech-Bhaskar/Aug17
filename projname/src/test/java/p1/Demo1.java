5ea8fd671d9c9cfc1a7fadb38ec34f9918436413
BhaskarPL <bhaskarpl@gmail.com>
2021-08-17 19:28:33 +0530

    My 1st Code on repo

---
+++ projname/src/test/java/p1/Demo1.java
@@ -0,0 +1,29 @@
+package p1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+		
+		WebDriverManager.firefoxdriver().setup();
+		
+		WebDriver driver1= new FirefoxDriver();
+		driver1.get("https://www.amazon.com");
+		
+		//driver.close();
+				
+		
+
+	}
+
+}
