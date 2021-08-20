d90fbb1c9ab195b6357088d6e7899f8ca3a9197f
BhaskarPL <bhaskarpl@gmail.com>
2021-08-20 21:51:50 +0530

    Changesmade

--- projname/src/test/java/generic/IConst.java
+++ projname/src/test/java/generic/IConst.java
@@ -2,5 +2,11 @@ package generic;
 
 public interface IConst {
 	int ITO=10;
-	String APP_URL="http://www.google.com";
+	String APP_URL="https://demo.actitime.com/login.do";
+	int ETO=10;
+	String XL_PATH="./data/input.xlsx";
+	String dURL="http://localhost:4444/wd/hub";
+	String dBrowser="chrome";
+	
+
 }
